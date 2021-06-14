2cd2e29f07942b5f0b540db827e4634504d90a77
Unruh-Spirale <protjan@wp.pl>
2021-06-14 16:16:45 +0200

    Finish working App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import {NgModule} from "@angular/core";
 import {Route, RouterModule} from "@angular/router";
 import {DashboardComponent} from "./currency/dashboard/dashboard.component";
+import {CurrencyResolveEurService} from "./currency/resolve-services/currency-resolve-eur.service";
+import {CurrencyResolveService} from "./currency/resolve-services/currency-resolve.service";
+import {CurrencyResolveChfService} from "./currency/resolve-services/currency-resolve-chf.service";
+import {CurrencyResolveGbpService} from "./currency/resolve-services/currency-resolve-gbp.service";
 
 const APP_ROUTES: Route[] = [
   {
     path: '', pathMatch: 'full', redirectTo: 'dashboard'
   },
   {
-    path: 'dashboard', component: DashboardComponent
+    path: 'dashboard',
+    component: DashboardComponent,
+    resolve:
+      {
+        eurMid: CurrencyResolveEurService,
+        usdMid: CurrencyResolveService,
+        chfMid: CurrencyResolveChfService,
+        gbpMid: CurrencyResolveGbpService
+      }
+
   }
 ]
 
--- src/app/currency/dashboard/dashboard.component.ts
+++ src/app/currency/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as Highcharts from 'highcharts';
+import {Table} from "../model/table";
+import {ActivatedRoute} from "@angular/router";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,9 +13,87 @@ export class DashboardComponent implements OnInit {
 
   title = 'DASHBOARD';
 
-  constructor() { }
+  eurMid: Table;
+  usdMid: Table;
+  chfMid: Table;
+  gbpMid: Table;
+
+  eur90: number[];
+  usd90: number[];
+  chf90: number[];
+  gbp90: number[];
+
+  constructor(private route: ActivatedRoute) { }
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'Average currencies rates from 90 days'
+    },
+    xAxis: {
+      categories: []
+    },
+    yAxis: {
+      title:{
+        text: 'rate'
+      }
+    },
+    series: []
+  }
+
 
   ngOnInit(): void {
+    this.loadData();
+    this.loadDataToCharts();
+  }
+
+  loadData(): void {
+    this.eurMid = this.route.snapshot.data['eurMid'];
+    this.usdMid = this.route.snapshot.data['usdMid'];
+    this.chfMid = this.route.snapshot.data['chfMid'];
+    this.gbpMid = this.route.snapshot.data['gbpMid'];
+
+    this.eur90 = this.eurMid.rates.map(x => x.mid);
+    this.usd90 = this.usdMid.rates.map(x => x.mid);
+    this.chf90 = this.chfMid.rates.map(x => x.mid);
+    this.gbp90 = this.gbpMid.rates.map(x => x.mid);
+  }
+
+  loadDataToCharts(): void{
+    this.chartOptions.xAxis = [
+      {
+        categories: this.eurMid.rates.map(x => x.effectiveDate)
+      }
+    ]
+    this.chartOptions.series = [
+      {
+        name: 'EUR',
+        data: this.eur90,
+        type: 'line',
+        color: 'gold'
+      },
+      {
+        name: 'USD',
+        data: this.usd90,
+        type: 'line',
+        color: 'green'
+      },
+      {
+        name: 'CHF',
+        data: this.chf90,
+        type: 'line',
+        color: 'red'
+      },
+      {
+        name: 'GBP',
+        data: this.gbp90,
+        type: 'line',
+        color: 'blue'
+      }
+    ]
   }
 
 }
